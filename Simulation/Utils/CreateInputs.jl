00d8e33d6c5824e4de973e0bc3af139eb2161d63
Leandro Martinez <leandro@iqm.unicamp.br>
2021-05-14 18:43:20 -0300

    added input_gen function to module

--- Simulation/Utils/CreateInputs.jl
+++ Simulation/Utils/CreateInputs.jl
@@ -24,6 +24,11 @@ v_cos(n,m) = (n*m*1e-3)/(6.02e23);
 # Number of water molecules - The number of water molecules is calculated occording to its molar mass and the volume avaiable (Box - Prot)
 num_wat(vs,vil) = round(Int,((vs - vil) * 6.02e23) / (18*1e-3));
 
+"""
+
+Function that generates an input file for Packmol and the topology file, from a base topology for the system.
+
+"""
 function box(pdbfile::String, solvent_file::String, concentration::Real, box_size::Real; 
              box_file="box.inp",
              topology_base="topology_base.top",
@@ -90,7 +95,6 @@ function box(pdbfile::String, solvent_file::String, concentration::Real, box_siz
                 """)
  end
  println("Wrote file: $box_file")
-  
 
  # Read base topology and write new topolgy with actual numbers
  open(topology_out,"w") do output
@@ -105,6 +109,45 @@ function box(pdbfile::String, solvent_file::String, concentration::Real, box_siz
  println("Wrote file: $topology_out\n")
 
 end # function box
+
+"""
+
+Given the output of the minimization, this function will generate the input
+of the equilibration and production runs
+
+"""
+function input_gen(nrep=4, Tₘ=425.0, T₀=300.0; 
+                   filesdir="./",
+                   processed="$filesdir/processed.top",
+                   topology="$filesdir/topology.top",
+                   minimization_out="$filesdir/minimization.gro")
+   
+  λ = [exp((-i/($nrep-1))*log($Tm/$T0)) for i in 0:($nrep-1)]
+  T = T₀ ./ λ
+  
+  for i in 0:(nrep-1)
+    run(`mkdir -p $i`)
+    run(`cp plumed.dat ./$i`)   
+    for file in ["nvt", "npt", "prod"]
+      open("$filesdir/$file.mdp","r") do input
+        open("$i/$file.tmp","w") do output
+          for line in eachline(input)
+            line = replace(line,"REFT" => $(T[i+1]))
+            println(output,line)
+          end
+        end
+      end
+    end
+         
+    # plumed partial-tempering calculation
+    # This is equivalent to bash: cat $processed > plumed partial_tempering $(λ[i+1]) > $out/$topology 
+    run(pipeline($processed, `plumed partial_tempering $(λ[i+1])`, "$i/$topology"))
+
+    # run Gromacs
+    run(`gmx_mpi grompp -f $i/nvt.mdp -c $minimization_out -p $i/$topology $i/canonical.tpr -maxwarn 3`)
+  end
+
+end # function input_gen
 
 end # module
 
