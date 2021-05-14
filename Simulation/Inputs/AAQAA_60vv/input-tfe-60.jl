b4280dfe8ce10978bef56d331732509344d93286
Leandro Martinez <leandro@iqm.unicamp.br>
2021-05-14 17:59:56 -0300

    added CreateInputs.jl

--- Simulation/Inputs/AAQAA_60vv/input-tfe-60.jl
+++ Simulation/Inputs/AAQAA_60vv/input-tfe-60.jl
@@ -1,7 +1,7 @@
 #
 # This script read pdb files and write inp inputs for using in packmol.
 #
-module CreateInput
+module CreateInputs
 
   using PDBTools
 
@@ -25,90 +25,91 @@ module CreateInput
   # Number of water molecules - The number of water molecules is calculated occording to its molar mass and the volume avaiable (Box - Prot)
   num_wat(vs,vil) = round(Int,((vs - vil) * 6.02e23) / (18*1e-3));
 
-  function box(pdbfile::String, solvent_file::String, concentration::Real, box_size::Vector{<:Real}; selection="all")
+  function box(pdbfile::String, solvent_file::String, concentration::Real, box_size::Real; 
+               box_file="box.inp",
+               topology_base="topology_base.top",
+               topology_out="topology.top",
+               water_file="tip4p2005.pdb")
 
-    protein = readPDB(pdbfile,selection)
+    protein = readPDB(pdbfile)
     solvent = readPDB(solvent_file)
 
-    protein_mass = mass(atoms)
+    protein_mass = mass(protein)
     solvent_mass = mass(solvent)
 
     # Box dimensions
-    lx, ly, lz .= box_size
+    l = maximum(maxmin(protein).xlength)/2 + box_size 
   
     # Solution volume (vbox - vprotein)
-    vs   = vol_box(2*lx,2*ly,2*lz) - vol_prot(protein_mass)
+    vs   = vol_box(2*l,2*l,2*l) - vol_prot(protein_mass)
   
-    # number of tfe molecules
-    ncos = num(vs,c) 
+    # number of solvent molecules
+    ncos = num(vs,concentration) 
     vcos = v_cos(ncos,solvent_mass)
   
     #number of water molecules
     nwat = num_wat(vs,vcos)
   
     println("""
-            Important data:
-            Box volume = $(vol_box(2*lx,2*ly,2*lz))
-            solution volume = $vs   
+
+            Summary:
+            ========
+            Box volume = $(vol_box(2*l,2*l,2*l))
+            Solution volume = $vs   
+            Protein molar mass = $protein_mass
+            Cossolvent molar mass = $solvent_mass
             Cossolvent volume = $vcos 
             Number of cossolvent molecules = $ncos 
             Volume of water molecules = $(vs-vcos)
             Number of water molecules = $nwat
             """)
   
-    io = open("box.inp","w")
-    println(io,"""
-                tolerance 2.0
-                output system.pdb
-                add_box_sides 1.0
-                filetype pdb
-                seed -1
+    
+    open(box_file,"w") do io
+      println(io,"""
+                  tolerance 2.0
+                  output system.pdb
+                  add_box_sides 1.0
+                  filetype pdb
+                  seed -1
 
-                structure $PDB
-                  number 1
-                  center
-                  fixed 0. 0. 0. 0. 0. 0.
-                end structure
+                  structure $pdbfile
+                    number 1
+                    center
+                    fixed 0. 0. 0. 0. 0. 0.
+                  end structure
 
-                structure tip4p2005.pdb
-                  number $nwat
-                  inside box -$lx -$ly -$lz $lx $ly $lz
-                end structure
+                  structure $water_file
+                    number $nwat
+                    inside box -$l -$l -$l $l $l $l
+                  end structure
 
-                structure $solvent_file
-                  number $ncos
-                  inside box -$lx -$ly -$lz $lx $ly $lz
-                end structure
-                """)
-    close(io)
+                  structure $solvent_file
+                    number $ncos
+                    inside box -$l -$l -$l $l $l $l
+                  end structure
+                  """)
+   end
+   println("Wrote file: $box_file")
+    
   
-   # topology  
-   file1 = open("topol_back.top","r")  
-   file2 = open("topol.top","w")
-   for line in eachline(file1)   
-     if occursin("NWAT",line) 
-       println(file2,replace(line,"NWAT" => "$nwat",count = 1))
-     elseif occursin("NCOS",line) 
-       println(file2,replace(line,"NCOS" => "$ncos",count = 1))
-     else
-       println(file2,line)
+   # Read base topology and write new topolgy with actual numbers
+   open(topology_out,"w") do output
+     open(topology_base,"r") do input
+       for line in eachline(input)
+         line = replace(line,"NWAT" => "$nwat") 
+         line = replace(line,"NCOS" => "$ncos")
+         println(output,line)
+       end
      end
    end
-   close(file1)
-   close(file2)
-  
-  end
+   println("Wrote file: $topology_out\n")
 
-end
+  end # function box
 
-using .CreateInput
+end # module
 
-protein_file = ARGS[1]
-solvent_file = ARGS[2]
-
-box(ARGS[1],ARGS[2],)
-
-box(1239.3588, "AAQAA.pdb", 6.)
+CreateInputs.box("AAQAA.pdb", "tfe.pdb", 6., 28., box_file="box.inp")
 
 
 
--- Simulation/README.md
+++ Simulation/README.md
@@ -7,9 +7,28 @@
 * [Equilibração da temperatura e da pressão](#equi)
 * [Produção - HREMD](#prod)
 
+## Defina um diretório onde tudo acontecerá, e crie esse diretório:
+
+```
+simulation_dir=/home/user/path/Simulations
+mkdir -p $simulation_dir
+```
+
+usaremos a variável `simulation_dir` daqui em diante como referência ao diretório escolhido.
+
 ## <a name="config"></a>Configuração inicial do sistema
 
 O primeiro passo para a realização da simulação é definir qual o sistema que será simulado. Para o nosso caso, iremos simular um sistema composto pelo polipeptídeo `(AAQAA)3` e por uma solução aquosa de 60%v/v do 2,2,2-Trifluoretanol (TFE). O Script que calcula as dimensões da caixa, assim como as quantidades de cada componente do solvente é o `input-tfe-60.jl`. Basicamente, este script irá calcular a quantidade de TFE e água necessários para atingir 6 mol/L (o que equivale a 60%v/v) em uma caixa de 56 &angstrom; de aresta.
+
+### Atalho
+
+Faça o download do arquivo [](createbox.sh). Em seguida, faça dele um executavel e rode com:
+
+```
+chmod +x createbox.sh
+createbox.sh $simulation_dir
+```
+
 
 (ADICIONAR OBSERVAÇÃO PARA OSISTEMA APENAR COM ÁGUA)
 
