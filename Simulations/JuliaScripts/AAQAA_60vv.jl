2f922e6bd58d46719fc406ef1614d6681d881298
Leandro Martinez <leandro@iqm.unicamp.br>
2021-05-18 11:08:12 -0300

    up docs

--- Simulations/JuliaScripts/AAQAA_60vv.jl
+++ Simulations/JuliaScripts/AAQAA_60vv.jl
@@ -8,21 +8,56 @@ script_dir = @__DIR__
 include("$script_dir/CreateInputs.jl")
 using .CreateInputs 
 
-data_dir="$script_dir/../InputData"
+# Density as a function of molar fraction of TFE
+#   FE mol frac  Density (g/mL) ref: https://doi.org/10.1023/A:1005147318013
+ρ = [ 0.00       0.99707       
+      0.00130    0.99931       
+      0.00178    1.00013       
+      0.00264    1.00161       
+      0.00318    1.00253       
+      0.00587    1.00711       
+      0.00792    1.01061       
+      0.00841    1.01145       
+      0.01362    1.02009       
+      0.01550    1.02331       
+      0.01966    1.03032       
+      0.02837    1.04416       
+      0.04430    1.06827       
+      0.06485    1.09668       
+      0.07171    1.10545       
+      0.08744    1.12449       
+      0.1071     1.14364       
+      0.1526     1.18115       
+      0.2126     1.22017       
+      0.2960     1.26039       
+      0.4245     1.30204       
+      0.6088     1.33868       
+      0.7820     1.36029       
+      0.9451     1.37873       
+      1.0000     1.38217  ]
 
+# What we want
+concentration = 50.0 #%vv
+
+# Find to what molar fraction this volume fraction corresponts
+x = CreateInputs.find_x(concentration, 100.4, 1.38217, ρ)
+println("Molar fraction = $x")
+
+# Iterpolate to get density given molar fraction
+density = interpolate(x,ρ)
+println("Density = $density")
+
+data_dir="$script_dir/../InputData"
 pdbfile = "$data_dir/PDB/AAQAA.pdb"
 solvent_file = "$data_dir/PDB/tfe.pdb"
 water_file = "$data_dir/PDB/tip4p2005.pdb"
-concentration = 6.0
 box_size = 56.
-topology_base = "$data_dir/Topology/topology_base_TFE.top"
 
 CreateInputs.box(pdbfile, solvent_file, concentration, box_size,
-                 density=1.33,
-                 box_file="box.inp",
-                 topology_base=topology_base,
-                 topology_out="topology.top",
-                 water_file=water_file)
-    
+                 water_file=water_file,
+                 density=density,
+                 density_pure_solvent=1.38217,
+                 box_file="box.inp",cunit="%vv")
+
 
 
--- Simulations/JuliaScripts/CreateInputs.jl
+++ Simulations/JuliaScripts/CreateInputs.jl
@@ -7,77 +7,241 @@ module CreateInputs
 using PDBTools
 
 # conversion factor from mL/mol to Å^3/molecule
-const convert_molar_volume = 1e24 / 6.02e23
+const CMV = 1e24 / 6.02e23   
 
 # conversion factor from mol/L to molecules/Å^3
-const convert_molar_concentration = 6.02e23 / 1e27  
-
-# Volume of the protein (converted to A 
-vol_prot(mass,density) = convert_molar_volume*mass/density
-
-# Volume of the solution
-vol_sol(vc,vp) = vc - vp
-
-# number of ionic liquids (or any other additional compound) molecules 
-num(vs,c) = round(Int,convert_molar_concentration*vs*c)
-
-# Volume of ionic liquids (or any other compound)  molecules
-v_cos(n,mass,density) = convert_molar_volume*n*mass*density
-
-# Number of water molecules - The number of water molecules is calculated occording to its molar mass and the volume avaiable (Box - Prot)
-num_wat(vs,vil,density) = round(Int,(vs - vil)/(convert_molar_volume*18/density))
+const CMC = 6.02e23 / 1e27   
 
 """
 
-Function that generates an input file for Packmol and the topology file, from a base topology for the system.
+Interpolate a value from a (x,y) data matrix
+
+"""
+function interpolate(x,ρ)  
+  i = findfirst(d -> d > x, ρ[:,1])
+  dρdx = (ρ[i,2]-ρ[i-1,2])/(ρ[i,1]-ρ[i-1,1])
+  d = ρ[i-1,2] + dρdx * (x - ρ[i-1,1])
+  return d
+end
+
+"""
+
+Find by bisection which is the molar fraction that is consistent
+with a desired volume fraction (%), given a data table of densities
+as a function of the molar fractions.
+
+"""
+function find_x(vv, cossolvent_mass, density_pure, densities::Matrix;
+                tol = 1e-5) 
+
+  xl = densities[1,1]
+  xr = densities[end,1]
+  if xl < xr 
+    increasing = true
+  else
+    increasing = false
+  end
+  x = (xr-xl)/2
+  xnew = convert_c(vv,"%vv" => "x", density=interpolate(x,densities),
+                   molar_mass=cossolvent_mass, density_pure=density_pure)
+  while abs(xnew-x) > tol 
+    if xnew > x
+      if increasing
+        xl = x 
+      else
+        xr = x
+      end
+    else
+      if increasing
+        xr = x 
+      else
+        xl = x 
+      end
+    end
+    x = (xr+xl)/2
+    xnew = convert_c(vv,"%vv" => "x", density=interpolate(x,densities),
+                     molar_mass=cossolvent_mass, density_pure=density_pure)
+  end
+  return x
+end
+
+"""
+
+Convert concentrations.
+
+"""
+function convert_c(cin,units;
+                   density=nothing, # solution
+                   density_pure=nothing, # cossolvent
+                   molar_mass=18., # cossolvent
+                   molar_mass_water=18.,
+                   density_water=1.0)
+
+  # Why not allow this
+  units[1] == units[2] && return cin
+
+  ρ = density
+  ρc = density_pure
+  ρw = density_water 
+  Mw = molar_mass_water
+  Mc = molar_mass
+
+  # nc and nw are the molar concentrations
+
+  if units[1] == "%vv"
+    if ρc == nothing
+      error("Density of pure solvent is required to convert from %vv.")
+    end
+    vv = cin/100
+    nc = ( ρc*vv / Mc )
+    if units[2] == "x"
+      if ρ == nothing
+        error("Density of solution is required to convert to molar fraction.")
+      end
+      nw = ( ρ - nc*Mc ) / Mw
+      return nc/(nc + nw)
+    end
+    if units[2] == "mol/L"
+      return 1000*nc
+    end
+  end
+
+  if units[1] == "x"
+    if ρ == nothing
+      error("Density of solution is required to convert from molar fraction.")
+    end
+    x = cin
+    nc = ρ / ( Mc + Mw*(1-x)/x )
+    if units[2] == "mol/L"
+      return 1000*nc
+    end
+    if units[2] == "%vv"
+      if ρc == nothing
+        error("Density of pure solvent is required to convert to %vv.")
+      end
+      nw = nc*(1-x)/x
+      Vc = nc * Mc / ρc
+      vv = 100*Vc
+      return vv
+    end
+  end
+
+  if units[1] == "mol/L"
+    if ρ == nothing
+      error("Density of solution is required to convert from molarity.")
+    end
+    nc = cin/1000 
+    nw = ( ρ - nc*Mc ) / Mw
+    if units[2] == "x"
+      return nc/(nc + nw)
+    end
+    if units[2] == "%vv"
+      if ρc == nothing
+        error("Density of pure solvent is required to convert to %vv.")
+        return nothing
+      end
+      Vc = nc * Mc / ρc 
+      vv = 100*Vc
+      return vv
+    end
+  end
+
+end
+
+"""
+
+Function that generates an input file for Packmol. By default, the concentrations is given in mol/L, but it can also be given in molar fraction "x" or volume percentage "%vv", using `cunit="x"` or `cunit="%vv"`. 
 
 """
 function box(pdbfile::String, solvent_file::String, concentration::Real, box_side::Real; 
+             water_file="tip4p2005.pdb",  
              density=1.0,
+             density_pure_solvent=nothing,
              box_file="box.inp",
-             topology_base="topology_base.top",
-             topology_out="topology.top",
-             water_file="tip4p2005.pdb")
+             cunit="mol/L")
 
   protein = readPDB(pdbfile)
-  solvent = readPDB(solvent_file)
+  cossolvent = readPDB(solvent_file)
 
-  protein_mass = mass(protein)
-  solvent_mass = mass(solvent)
+  # molar masses (g/mol)
+  Mp = mass(protein)
+  Mc = mass(cossolvent)
+  Mw = 18.01528
 
-  # Box volue
+  # aliases for clearer formulas
+  ρ = density
+  ρc = density_pure_solvent
+
+  # Convert concentration to mol/L
+  cc = convert_c(concentration, cunit => "mol/L",
+                 density=ρ,
+                 density_pure=ρc,
+                 molar_mass=Mc)
+  c_vv = convert_c(concentration, cunit => "%vv",
+                   density=ρ,
+                   density_pure=ρc,
+                   molar_mass=Mc)
+  c_x = convert_c(concentration, cunit => "x",
+                  density=ρ,
+                  density_pure=ρc,
+                  molar_mass=Mc)
+
+  # Convert cossolvent concentration in molecules/Å³
+  cc = CMC*cc
+
+  # Box volume (Å³)
   vbox = box_side^3
 
   # Solution volume (vbox - vprotein)
-  vs = vbox - vol_prot(protein_mass,density)
+  vs = vbox - CMV*Mp/ρ
 
-  # number of solvent molecules
-  ncos = num(vs,concentration)
-  vcos = v_cos(ncos,solvent_mass,density)
+  # number of cossolvent molecules
+  nc = round(Int,cc*vs)
 
   #number of water molecules
-  nwat = num_wat(vs,vcos,density)
+  nw = round(Int,(ρ*vs/CMV - nc*Mc)/Mw)
 
   # Final density of the solution
-  ρ = convert_molar_volume*(solvent_mass*ncos + 18*nwat)/vs
+  ρ = CMV*(Mc*nc + Mw*nw)/vs
+  
+  # Final cossolvent concentration (mol/L)
+  cc_f = 1000*(nc/vs)*CMV
+
+  # Final water concentration (mol/L)
+  cw_f = 1000*(nw/vs)*CMV
+
+  # Final recovered concentration in %vv
+  vv = 100*CMV*(nc*Mc/ρc)/vs
 
   println("""
 
           Summary:
           ========
+          Target concentration = $cc mol/L
+                               = $c_vv %vv
+                               = $c_x x
+
           Box volume = $vbox Å³
           Solution volume = $vs Å³   
-          Protein molar mass = $protein_mass g/mol
-          Cossolvent molar mass = $solvent_mass g/mol
-          Volume for cossolvent = $vcos Å³ 
-          Number of cossolvent molecules = $ncos molecules
-          Volume for water molecules = $(vs-vcos) Å³
-          Number of water molecules = $nwat molecules
 
+          Density = $ρ g/mL
+          Protein molar mass = $Mp g/mol
+          Cossolvent molar mass = $Mc g/mol
+
+          Concentration = $concentration mol/L
+                        = $cc molecules/Å³
+
+          Number of cossolvent molecules = $nc molecules
+          Number of water molecules = $nw molecules
+
+          Final cossolvent concentration = $cc_f mol/L
+          Final water concentration = $cw_f mol/L
+                                    = $(CMC*cw_f) molecules/Å³
           Final solvent density = $ρ g/mL
+          Final %vv concentration = $vv %
+          Final molar fraction = $(nc/(nc+nw))
           """)
 
-  
   l = box_side/2
   open(box_file,"w") do io
     println(io,"""
@@ -94,14 +258,14 @@ function box(pdbfile::String, solvent_file::String, concentration::Real, box_sid
                end structure
 
                structure $water_file
-                 number $nwat
+                 number $nw
                  inside box -$l -$l -$l $l $l $l
                end structure
                """)
-      if ncos > 0 
+      if nc > 0 
         println(io,"""
                 structure $solvent_file
-                  number $ncos
+                  number $nc
                   inside box -$l -$l -$l $l $l $l
                 end structure
                 """)
