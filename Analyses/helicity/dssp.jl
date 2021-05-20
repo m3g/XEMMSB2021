46105a0f826f26304bbaad67eef22c48b67c3326
Leandro Martinez <leandro@iqm.unicamp.br>
2021-05-20 18:00:38 -0300

    up

--- Analyses/helicity/dssp.jl
+++ Analyses/helicity/dssp.jl
@@ -1,74 +1,57 @@
-using Plots, LaTeXStrings, DelimitedFiles, Statistics
+using Plots, Statistics
 
-    file = open("./resid", "w")
-    
-    println(file,1)
-    println(file,2)
-    println(file,3)
-    println(file,4)
-    println(file,5)
-    println(file,6)
-    println(file,7)
-    println(file,8)
-    println(file,9)
-    println(file,10)
-    println(file,11)
-    println(file,12)
-    println(file,13)
-    println(file,14)
-
+function get_helicity(dir,nresidues)
+  files = filter(f -> f[end-4:end] == ".dssp", readdir(dir))
+  nfiles = length(files)  
+  helix = zeros(Int,nfiles,nresidues)
+  for i in 0:nfiles-1
+    file = open("$dir/dssp$i.pdb.dssp","r")
+    residue = -1
+    for line in eachline(file)
+      # skip header
+      if occursin("RESIDUE",line)
+        residue = 0
+        continue
+      end
+      if residue == -1
+        continue
+      end
+      # read data
+      residue += 1
+      if line[17:17] == "H"
+        helix[i+1,residue] = 1
+      end
+    end
     close(file)
+  end
+  avg_helicity_per_frame = [ mean(helix[i,:]) for i in 1:nfiles ]
+  avg_helicity_per_residue = [ mean(helix[:,i]) for i in 1:nresidues ]
+  return nfiles, avg_helicity_per_frame, avg_helicity_per_residue
+end
 
-matrix0vv = readdlm("./matrix.xvg")
-matrix20vv = readdlm("../20vv/matrix.xvg")
+nresidues = 18
 
-    file0vv = open("./helix0vv", "w")
+# pure water
+nfiles_pure, pure_per_frame, pure_per_residue = get_helicity("./AAQAA_0vv/0/DSSP",nresidues)
 
-    println(file0vv,mean(matrix0vv[1,:]))
-    println(file0vv,mean(matrix0vv[2,:]))
-    println(file0vv,mean(matrix0vv[3,:]))
-    println(file0vv,mean(matrix0vv[4,:]))
-    println(file0vv,mean(matrix0vv[5,:]))
-    println(file0vv,mean(matrix0vv[6,:]))
-    println(file0vv,mean(matrix0vv[7,:]))
-    println(file0vv,mean(matrix0vv[8,:]))
-    println(file0vv,mean(matrix0vv[9,:]))
-    println(file0vv,mean(matrix0vv[10,:]))
-    println(file0vv,mean(matrix0vv[11,:]))
-    println(file0vv,mean(matrix0vv[12,:]))
-    println(file0vv,mean(matrix0vv[13,:]))
-    println(file0vv,mean(matrix0vv[14,:]))
+# with TFE
+nfiles_tfe, tfe_per_frame, tfe_per_residue = get_helicity("./AAQAA_0vv/0/DSSP",nresidues)
 
-    close(file0vv)
 
-    file20vv = open("../20vv/helix20vv", "w")
+# plot
+default(fontfamily="Computer Modern",linewidth=2,framestyle=:box)
+plot(layout=(2,1))
 
-    println(file20vv,mean(matrix20vv[1,:]))
-    println(file20vv,mean(matrix20vv[2,:]))
-    println(file20vv,mean(matrix20vv[3,:]))
-    println(file20vv,mean(matrix20vv[4,:]))
-    println(file20vv,mean(matrix20vv[5,:]))
-    println(file20vv,mean(matrix20vv[6,:]))
-    println(file20vv,mean(matrix20vv[7,:]))
-    println(file20vv,mean(matrix20vv[8,:]))
-    println(file20vv,mean(matrix20vv[9,:]))
-    println(file20vv,mean(matrix20vv[10,:]))
-    println(file20vv,mean(matrix20vv[11,:]))
-    println(file20vv,mean(matrix20vv[12,:]))
-    println(file20vv,mean(matrix20vv[13,:]))
-    println(file20vv,mean(matrix20vv[14,:]))
+# helicity as a function of time
+sp=1
+plot!(1:nfiles_pure,pure_per_frame,label="pure water",xticks=1:nfiles_pure,subplot=sp)
+plot!(1:nfiles_tfe,tfe_per_frame,label="tfe",xticks=1:nfiles_tfe,subplot=sp)
+plot!(xlabel="frame",ylabel="α-helical content",subplot=sp)
 
-    close(file20vv)
-
-    resid = readdlm("./resid")
- helix0vv = readdlm("./helix0vv")
-helix20vv = readdlm("../20vv/helix20vv")
-
-plot_font = "Computer Modern"
-default(fontfamily=plot_font)
-
-plot(resid,helix0vv,c=:orangered1,label=L"\mathrm{(AAQAA)_3 \ in \ Water}",ylabel=L"\mathrm{\alpha-helix \ fraction}",xlabel="Residue Number",w=3,framestyle=:box,dpi=300,xtickfontsize=18,ytickfontsize=18,xguidefontsize=18,yguidefontsize=18,legendfontsize=10,minorticks=Integer,ylim=[0.,1.],legend=:topright)
-
-plot!(resid,helix20vv,c=:blue,label=L"\mathrm{(AAQAA)_3 \ in \ TFE}",ylabel=L"\mathrm{\alpha-helix \ fraction}",xlabel="Residue Number",w=3,framestyle=:box,dpi=300,xtickfontsize=18,ytickfontsize=18,xguidefontsize=18,yguidefontsize=18,legendfontsize=10,minorticks=Integer,ylim=[0.,1.], legend=:topright)
+# helicity per residue 
+sp=2
+plot!(1:nresidues,pure_per_residue,label="pure water",xticks=1:nresidues,subplot=sp)
+plot!(1:nresidues,tfe_per_residue,label="tfe",xticks=1:nresidues,subplot=sp)
+plot!(xlabel="residue",ylabel="α-helical content",subplot=sp)
 
 savefig("./helicity.png")
--- Analyses/helicity/run-dssp.sh
+++ Analyses/helicity/run-dssp.sh
@@ -1,51 +1,15 @@
 #!/bin/bash
 
-# Gerar os arquivos .pdb de cada frame da simulação
-echo 1 | gmx trjconv -f production-center.xtc -s production.tpr -o $1.pdb -sep
+mkdir -p ./DSSP
 
-# Calcular os elementos de estrutura secundária com o software DSSP
-       for i in {0..1000}; do
-       
-         mkdssp -i $i.pdb -o $i.dssp
+# Create PDB files for each frame
+echo 1 | gmx_mpi trjconv -f production.xtc -s production.tpr -o ./DSSP/dssp$1.pdb -sep
 
-       done
-
-# Recortar o arquivos .dssp
-       for i in {0..1000}; do
-       
-         sed -n 29,42p $i.dssp > $i.txt
-
-       done
-
-       for i in {0..1000};do
-
-         cat $i.txt | awk '{print $4}' > $i.dat
-
-       done
-
-# Construir uma matriz com M linhas (14 linhas, que reflete o número de resíduos do peptídeo) e N colunas (1001, que é igual ao número de frames da simulação)
-       for i in {0..1000};do
-
-         paste {0..1000}.dat > matrix-all.xvg
-
-       done
-
-# Backup do arquivo matrix-correct.xvg
-       cp matrix-all.xvg matrix.xvg
-
-# Contruir uma matriz de 0 (para qualquer atribuição diferente de H) e 1 (quando o resíduo for atribuído como H) 
-       sed -i 's/H/1/g' matrix.xvg
-       sed -i 's/G/0/g' matrix.xvg
-       sed -i 's/I/0/g' matrix.xvg
-       sed -i 's/T/0/g' matrix.xvg
-       sed -i 's/E/0/g' matrix.xvg
-       sed -i 's/B/0/g' matrix.xvg
-       sed -i 's/S/0/g' matrix.xvg
-       sed -i 's/C/0/g' matrix.xvg
-       sed -i 's/>/0/g' matrix.xvg
-       sed -i 's/</0/g' matrix.xvg
-       sed -i 's/+/0/g' matrix.xvg
-       sed -i 's/-/0/g' matrix.xvg
-       sed -i 's/X/0/g' matrix.xvg
-
+# Compute secondary structure
+cd DSSP
+for file in `ls dssp*.pdb`; do
+  # Compute secondary structure
+  mkdssp -i $file -o $file.dssp
+done
+cd ..
 
