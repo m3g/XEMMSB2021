ec23041005a98c2f4cd0140c634b442bedb81721
Leandro Martinez <leandro@iqm.unicamp.br>
2021-05-20 21:45:23 -0300

    up

--- Analyses/helicity/dssp.jl
+++ Analyses/helicity/dssp.jl
@@ -1,7 +1,7 @@
-using Plots, Statistics
+using Plots, Statistics, PDBTools
 
 function get_helicity(dir,nresidues)
-  files = readlines(`bash -c 'ls $dir/*.dssp'`)
+  files = filter(f -> f[end-4:end] == ".dssp", readdir(dir))
   nfiles = length(files)  
   helix = zeros(Int,nfiles,nresidues)
   for i in 0:nfiles-1
@@ -29,13 +29,24 @@ function get_helicity(dir,nresidues)
   return nfiles, avg_helicity_per_frame, avg_helicity_per_residue
 end
 
-nresidues = 18
+# working directory
+work = ARGS[1]
+if ! isdir(work)
+  error("Run with: dssp.jl \$work")
+end
+
+# Number or residues of peptide
+peptide = readPDB("$work/Simulations/AAQAA_0vv/system.pdb","protein")
+sequence = getseq(peptide)
+nresidues = length(sequence)
 
 # pure water
-nfiles_pure, pure_per_frame, pure_per_residue = get_helicity("./AAQAA_0vv/0/DSSP",nresidues)
+nfiles_pure, pure_per_frame, pure_per_residue = 
+  get_helicity("$work/Simulations/AAQAA_0vv/0/DSSP",nresidues)
 
 # with TFE
-nfiles_tfe, tfe_per_frame, tfe_per_residue = get_helicity("./AAQAA_0vv/0/DSSP",nresidues)
+nfiles_tfe, tfe_per_frame, tfe_per_residue = 
+  get_helicity("$work/Simulations/AAQAA_0vv/0/DSSP",nresidues)
 
 
 # plot
@@ -44,14 +55,21 @@ plot(layout=(2,1))
 
 # helicity as a function of time
 sp=1
-plot!(1:nfiles_pure,pure_per_frame,label="pure water",xticks=1:nfiles_pure,subplot=sp)
-plot!(1:nfiles_tfe,tfe_per_frame,label="tfe",xticks=1:nfiles_tfe,subplot=sp)
-plot!(xlabel="frame",ylabel="α-helical content",subplot=sp)
+plot!(1:nfiles_pure,pure_per_frame,label="Water",subplot=sp)
+plot!(1:nfiles_tfe,tfe_per_frame,label="Water/TFE",subplot=sp)
+plot!(xlabel="frame",
+      ylabel="α-helical content (%)",
+      xticks=1:nfiles_pure,
+      subplot=sp)
 
 # helicity per residue 
 sp=2
-plot!(1:nresidues,pure_per_residue,label="pure water",xticks=1:nresidues,subplot=sp)
-plot!(1:nresidues,tfe_per_residue,label="tfe",xticks=1:nresidues,subplot=sp)
-plot!(xlabel="residue",ylabel="α-helical content",subplot=sp)
+plot!(1:nresidues,100*pure_per_residue,label="Water",subplot=sp)
+plot!(1:nresidues,100*tfe_per_residue,label="Water/TFE",subplot=sp)
+plot!(xlabel="residue",
+      ylabel="α-helical content (%)",
+      xticks=(1:nresidues,["$(sequence[i])$i" for i in 1:nresidues]),
+      xrotation=60,
+      subplot=sp)
 
-savefig("./helicity.png")
+savefig("$work/Simulations/helicity.pdf")
