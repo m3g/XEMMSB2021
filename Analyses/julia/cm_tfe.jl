50d2866bbb7684053cdee3c60a65967287bd795c
Leandro Martinez <leandro@iqm.unicamp.br>
2021-05-23 10:09:31 -0300

    up

--- Analyses/julia/cm_tfe.jl
+++ Analyses/julia/cm_tfe.jl
@@ -1,8 +1,14 @@
 # Load packages
 using PDBTools, ComplexMixtures 
 
+# Repository dir
+if ! isdir(ARGS[1])
+  println("Run with: cm_tfe.jl \$repo") 
+  exit()
+end
+
 # Load PDB file of the system
-atoms = readPDB("../system.pdb")
+atoms = readPDB("$repo/Simulations/Final/system60.pdb")
 
 # Select the protein and the solvents
 protein = select(atoms,"protein")
@@ -15,7 +21,7 @@ solute = Selection(protein,nmols=1)
 solvent = Selection(tfe,natomspermol=9)
 
 # Setup the Trajectory structure
-trajectory = Trajectory("production.xtc",solute,solvent)
+trajectory = Trajectory("$repo/Simulations/Final/production60.xtc",solute,solvent)
 
 # Options (dbulk: distance above which the solute does not
 # affect the structure of the solvent)
