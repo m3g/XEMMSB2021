152caa44fa16f597e382c78942db35a597f963a5
Leandro Martinez <leandro@iqm.unicamp.br>
2021-05-23 14:44:46 -0300

    up

--- Analyses/julia/mddf-kb-tfe.jl
+++ Analyses/julia/mddf-kb-tfe.jl
@@ -1,4 +1,4 @@
-using Plots, LaTeXStrings, ComplexMixtures, PDBTools
+using Plots, LaTeXStrings, ComplexMixtures, PDBTools, EasyFit
 
 if (!isdir(ARGS[1])) || (!isdir(ARGS[2])) 
   println("Run with: julia mddf_kb.jl \$repo \$work")
@@ -21,32 +21,32 @@ results = ComplexMixtures.load("$work/Simulations/cm_tfe60.json")
 default(fontfamily="Computer Modern",grid=false,framestyle=:box,linewidth=2)
 
 # Complete MDDF
-plot(results.d,results.mddf,xlabel="r/Å",ylabel="mddf",label="Total")
+plot(results.d,movingaverage(results.mddf,10),xlabel="r/Å",ylabel="mddf",label="Total")
 
 # Fluorine atoms
 f_contrib = contrib(solvent,results.solvent_atom,
                     select(atoms,"resname TFE and element F"))
-plot!(results.d,f_contrib,label="Fluorine")
+plot!(results.d,movingaverage(f_contrib,10),label="Fluorine")
 
 # Carbon atoms
 c_contrib = contrib(solvent,results.solvent_atom,
                     select(atoms,"resname TFE and element C"))
-plot!(results.d,c_contrib,label="Carbon")
+plot!(results.d,movingaverage(c_contrib,10),label="Carbon")
 
 # Aliphatic hydrogens
 ha_contrib = contrib(solvent,results.solvent_atom,
                      select(atoms,"name H21 or name H22"))
-plot!(results.d,ha_contrib,label="Aliphatic H")
+plot!(results.d,movingaverage(ha_contrib,10),label="Aliphatic H")
 
 # Hydroxyl hydrogen
 hy_contrib = contrib(solvent,results.solvent_atom,
                      select(atoms,"resname TFE and name H"))
-plot!(results.d,hy_contrib,label="Hydroxyl H")
+plot!(results.d,movingaverage(hy_contrib,10),label="Hydroxyl H")
 
 # Hydroxyl oxygen
 ho_contrib = contrib(solvent,results.solvent_atom,
                      select(atoms,"resname TFE and name O"))
-plot!(results.d,hy_contrib,label="Hydroxyl O")
+plot!(results.d,movingaverage(hy_contrib,10),label="Hydroxyl O")
 
 # Save figure
 savefig("$work/Simulations/mddf-tfe.pdf")
