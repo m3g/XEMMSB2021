f9606922b5cbb89a8a9ccb02abb124001820dcc0
Leandro Martinez <leandro@iqm.unicamp.br>
2021-05-23 14:47:13 -0300

    up

--- Analyses/julia/mddf-kb-tfe.jl
+++ Analyses/julia/mddf-kb-tfe.jl
@@ -20,33 +20,36 @@ results = ComplexMixtures.load("$work/Simulations/cm_tfe60.json")
 # Default plot parameters
 default(fontfamily="Computer Modern",grid=false,framestyle=:box,linewidth=2)
 
+# Will use moving averages for more pretty graphs
+ma(data) = movingaverage(data,10).x
+
 # Complete MDDF
-plot(results.d,movingaverage(results.mddf,10),xlabel="r/Å",ylabel="mddf",label="Total")
+plot(results.d,ma(results.mddf),xlabel="r/Å",ylabel="mddf",label="Total")
 
 # Fluorine atoms
 f_contrib = contrib(solvent,results.solvent_atom,
                     select(atoms,"resname TFE and element F"))
-plot!(results.d,movingaverage(f_contrib,10),label="Fluorine")
+plot!(results.d,ma(f_contrib),label="Fluorine")
 
 # Carbon atoms
 c_contrib = contrib(solvent,results.solvent_atom,
                     select(atoms,"resname TFE and element C"))
-plot!(results.d,movingaverage(c_contrib,10),label="Carbon")
+plot!(results.d,ma(c_contrib),label="Carbon")
 
 # Aliphatic hydrogens
 ha_contrib = contrib(solvent,results.solvent_atom,
                      select(atoms,"name H21 or name H22"))
-plot!(results.d,movingaverage(ha_contrib,10),label="Aliphatic H")
+plot!(results.d,ma(ha_contrib),label="Aliphatic H")
 
 # Hydroxyl hydrogen
 hy_contrib = contrib(solvent,results.solvent_atom,
                      select(atoms,"resname TFE and name H"))
-plot!(results.d,movingaverage(hy_contrib,10),label="Hydroxyl H")
+plot!(results.d,ma(hy_contrib),label="Hydroxyl H")
 
 # Hydroxyl oxygen
 ho_contrib = contrib(solvent,results.solvent_atom,
                      select(atoms,"resname TFE and name O"))
-plot!(results.d,movingaverage(hy_contrib,10),label="Hydroxyl O")
+plot!(results.d,ma(hy_contrib),label="Hydroxyl O")
 
 # Save figure
 savefig("$work/Simulations/mddf-tfe.pdf")
