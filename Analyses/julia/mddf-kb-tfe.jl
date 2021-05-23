6bf743cd22b524694c7d90fca5533f16356e0066
Leandro Martinez <leandro@iqm.unicamp.br>
2021-05-23 14:40:40 -0300

    up

--- Analyses/julia/mddf-kb-tfe.jl
+++ Analyses/julia/mddf-kb-tfe.jl
@@ -7,7 +7,14 @@ end
 repo = ARGS[1]
 work = ARGS[2]
 
+# Set solute and solvent
 atoms = readPDB("$repo/Simulations/Final/system60.pdb")
+protein = select(atoms,"protein")
+tfe = select(atoms,"resname TFE")
+solute = Selection(protein,nmols=1) 
+solvent = Selection(tfe,natomspermol=9) 
+
+# Load previously calculated results
 results = ComplexMixtures.load("$work/Simulations/cm_tfe60.json")
 
 # Default plot parameters
@@ -17,27 +24,27 @@ default(fontfamily="Computer Modern",grid=false,framestyle=:box,linewidth=2)
 plot(results.d,results.mddf,xlabel="r/Å",ylabel="mddf",label="Total")
 
 # Fluorine atoms
-f_contrib = contrib(results,results.solvent_atom,
+f_contrib = contrib(solvent,results.solvent_atom,
                     select(atoms,"resname TFE and element F"))
 plot!(results.d,f_contrib,label="Fluorine")
 
 # Carbon atoms
-c_contrib = contrib(results,results.solvent_atom,
+c_contrib = contrib(solvent,results.solvent_atom,
                     select(atoms,"resname TFE and element C"))
 plot!(results.d,c_contrib,label="Carbon")
 
 # Aliphatic hydrogens
-ha_contrib = contrib(results,results.solvent_atom,
+ha_contrib = contrib(solvent,results.solvent_atom,
                      select(atoms,"name H21 or name H22"))
 plot!(results.d,ha_contrib,label="Aliphatic H")
 
 # Hydroxyl hydrogen
-hy_contrib = contrib(results,results.solvent_atom,
+hy_contrib = contrib(solvent,results.solvent_atom,
                      select(atoms,"resname TFE and name H"))
 plot!(results.d,hy_contrib,label="Hydroxyl H")
 
 # Hydroxyl oxygen
-ho_contrib = contrib(results,results.solvent_atom,
+ho_contrib = contrib(solvent,results.solvent_atom,
                      select(atoms,"resname TFE and name O"))
 plot!(results.d,hy_contrib,label="Hydroxyl O")
 
